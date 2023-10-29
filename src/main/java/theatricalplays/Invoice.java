8678ea3127398588247f3263c5fa45d2a93afe88
ibrahimacoulibaly58 <139682621+ibrahimacoulibaly58@users.noreply.github.com>
2023-10-29 15:02:39 +0100

    Ajout de Invoice.java

--- src/main/java/theatricalplays/Invoice.java
+++ src/main/java/theatricalplays/Invoice.java
@@ -4,11 +4,19 @@ import java.util.List;
 
 public class Invoice {
 
-  public String customer;
-  public List<Performance> performances;
+    private final String customer;
+    private final List<Performance> performances;
 
-  public Invoice(String customer, List<Performance> performances) {
-    this.customer = customer;
-    this.performances = performances;
-  }
+    public Invoice(String customer, List<Performance> performances) {
+        this.customer = customer;
+        this.performances = performances;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public List<Performance> getPerformances() {
+        return performances;
+    }
 }
--- src/main/java/theatricalplays/StatementPrinter.java
+++ src/main/java/theatricalplays/StatementPrinter.java
@@ -1,52 +1,59 @@
 package theatricalplays;
 
 import java.text.NumberFormat;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 public class StatementPrinter {
 
-  public String print(Invoice invoice, Map<String, Play> plays) {
-    int totalAmount = 0;
-    int volumeCredits = 0;
-    String result = String.format("Statement for %s\n", invoice.customer);
+    public String print(Invoice invoice, Map<String, Play> plays) {
+        int totalAmount = 0;
+        int volumeCredits = 0;
+        StringBuilder result = new StringBuilder();
+        result.append("Statement for ").append(invoice.getCustomer()).append("\n");
 
-    NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
-    for (Performance perf : invoice.performances) {
-      Play play = plays.get(perf.playID);
-      int thisAmount = 0;
+        for (Performance perf : invoice.getPerformances()) {
+            Play play = plays.get(perf.getPlayID());
+            int thisAmount = calculateAmount(play.getType(), perf.getAudience());
 
-      switch (play.type) {
-        case "tragedy":
-          thisAmount = 40000;
-          if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30);
-          }
-          break;
-        case "comedy":
-          thisAmount = 30000;
-          if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
-          }
-          thisAmount += 300 * perf.audience;
-          break;
-        default:
-          throw new Error("unknown type: ${play.type}");
-      }
+            // add volume credits
+            volumeCredits += Math.max(perf.getAudience() - 30, 0);
+            // add extra credit for every ten comedy attendees
+            if ("comedy".equals(play.getType())) volumeCredits += Math.floor(perf.getAudience() / 5);
 
-      // add volume credits
-      volumeCredits += Math.max(perf.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
-      if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+            // print line for this order
+            result.append("  ").append(play.getName()).append(": ").append(frmt.format(thisAmount / 100))
+                    .append(" (").append(perf.getAudience()).append(" seats)\n");
+            totalAmount += thisAmount;
+        }
+        result.append("Amount owed is ").append(frmt.format(totalAmount / 100)).append("\n");
+        result.append("You earned ").append(volumeCredits).append(" credits\n");
 
-      // print line for this order
-      result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
-      totalAmount += thisAmount;
+        return result.toString();
     }
-    result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
-    result += String.format("You earned %s credits\n", volumeCredits);
-    return result;
-  }
 
+    private int calculateAmount(String playType, int audience) {
+        int baseAmount = 0;
+        switch (playType) {
+            case "tragedy":
+                baseAmount = 40000;
+                if (audience > 30) {
+                    baseAmount += 1000 * (audience - 30);
+                }
+                break;
+            case "comedy":
+                baseAmount = 30000;
+                if (audience > 20) {
+                    baseAmount += 10000 + 500 * (audience - 20);
+                }
+                baseAmount += 300 * audience;
+                break;
+            default:
+                throw new Error("unknown type: " + playType);
+        }
+        return baseAmount;
+    }
 }
